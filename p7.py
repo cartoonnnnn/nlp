f80b68fbe77b3f0e60823d60fd169eb9bde92e17
cartoonnnnn <cartoonanime2696@gmail.com>
2026-04-30 10:27:55 +0530

    Update p7.py

--- p7.py
+++ p7.py
@@ -3,7 +3,11 @@ sudo apt install redis-server -y
 sudo service redis-server start
 redis-cli
 
-SET employee:1 '{"Name":"Rahul","Department":"HR","Position":"Manager","Salary":60000}' SET employee:2 '{"Name":"Anita","Department":"Finance","Position":"Analyst","Salary":55000}' SET employee:3 '{"Name":"Kiran","Department":"HR","Position":"Executive","Salary":48000}' SET employee:4 '{"Name":"Sneha","Department":"IT","Position":"Developer","Salary":70000}' SET employee:5 '{"Name":"Arjun","Department":"HR","Position":"Assistant","Salary":52000}'
+SET employee:1 '{"Name":"Rahul","Department":"HR","Position":"Manager","Salary":60000}'
+SET employee:2 '{"Name":"Anita","Department":"Finance","Position":"Analyst","Salary":55000}'
+SET employee:3 '{"Name":"Kiran","Department":"HR","Position":"Executive","Salary":48000}'
+SET employee:4 '{"Name":"Sneha","Department":"IT","Position":"Developer","Salary":70000}'
+SET employee:5 '{"Name":"Arjun","Department":"HR","Position":"Assistant","Salary":52000}'
 
 SADD dept:HR employee:1 employee:3 employee:5 SADD dept:Finance employee:2 SADD dept:IT employee:4
 
